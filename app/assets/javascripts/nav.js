d124b611b9e23a278493c948bb13fcd24848370e
Michael Minogue <michael.minogue@gmail.com>
2014-12-18 22:36:45 -0500

    Fix nav show/hide

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,11 +1,17 @@
 jQuery(document).ready(function() {
 	jQuery('.house').hide();
-	jQuery('#nav-animals').click(function(){
+	jQuery('#nav-animals').click(function(e){
+		jQuery('li').removeClass('active');
+		jQuery(this).addClass('active');
 		jQuery('.animals').show();
 		jQuery('.house').hide();
+		e.preventDefault();
 	});
-	jQuery('#nav-house').click(function(){
+	jQuery('#nav-house').click(function(e){
+		jQuery('li').removeClass('active');
+		jQuery(this).addClass('active');
 		jQuery('.animals').hide();
 		jQuery('.house').show();
+		e.preventDefault();
 	});
 });
